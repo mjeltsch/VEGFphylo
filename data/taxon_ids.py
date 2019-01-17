ce31a8d86ded54302a24bcf0c57708900204176b
Michael Jeltsch <michael@jeltsch.org>
2019-01-17 10:06:57 +0200

    Added lungfishes, changed sarcopterygii to actinista, backup taxon_ids.py before overwriting, deleted gi lists for internal nodes (mammalia, arthropoda, etc.)

--- data/taxon_ids.py
+++ data/taxon_ids.py
@@ -15,7 +15,8 @@
 #
 # sacropterygii: txid8287[Organism] NOT txid32523[Organism]
 # The number of species is off, because it counts still all tetrapods
-# One could substract tetrapods, which would leave 8 species
+# One could substract tetrapods, which would leave 8 species => replaced
+# by actinista (!= lobe-fined fishes, because Homo is highly derived lobe finned fish)
 #
 # The arthropod gi list for local analysis contains bacterial contamination (3064
 # sequences from Morganella morganii)
@@ -31,56 +32,8 @@
 # Removed because of too big gi id filesize (github accepts max. 100 MB):
 #
 #'tetrapoda': [32523, 27063, ''],
-{'mollusca': [6447, 13501, '']
-, 'hexapoda': [6960, 106986, 'insects']
-, 'platyhelminthes': [6157, 4171, 'flatworms']
-, 'priapulida': [33467, 7, 'penis worms']
-, 'crustacea': [6657, 10373, '']
-, 'aves': [8782, 9339, 'birds']
-, 'arachnida': [6854, 9644, 'spiders']
-, 'lepidosauria': [8504, 6803, 'lizards/snakes']
-, 'mammalia': [40674, 5022, '']
-, 'dicyemida': [10215, 24, '?']
-, 'orthonectida': [33209, 2, '?']
-, 'onychophora': [2074142, 89, 'velvet worms']
-, 'amphibia': [8292, 5520, '']
-, 'arthropoda': [6656, 128142, '']
-, 'eutheria': [9347, 4693, 'placentals']
-, 'echinodermata': [7586, 1724, 'sea stars/urchins/cucumbers/lilies']
-, 'myriapoda': [61985, 949, 'millipeds']
-, 'crocodylia': [1294634, 24, 'crocodiles']
-, 'micrognathozoa': [195505, 1, '?']
-, 'actinopterygii': [7898, 18329, 'ray-finned fishes']
-, 'metatheria': [9263, 324, 'marsupials']
-, 'annelida': [6340, 3170, 'segmented worms']
-, 'phoroniformea': [120557, 14, 'horseshoe worms']
-, 'rotifera': [10190, 233, 'wheel animals']
-, 'ctenophora': [1003038, 57, 'comb jellies']
-, 'chondrichthyes': [7777, 812, 'cartilaginous fishes']
-, 'gastrotricha': [33313, 118, 'hairybacks']
-, 'gnathostomulida': [66780, 21, 'jaw worms']
-, 'sarcopterygii': [8287, 27071, 'lobe-finned fishes']
-, 'kinorhyncha': [51516, 56, 'mud dragons']
-, 'nematomorpha': [33310, 28, 'horsehair worms']
-, 'hemichordata': [10219, 39, 'acorn wormws']
-, 'testudines': [8459, 353, 'turtles']
-, 'xenacoelomorpha': [1312402, 150, '?']
-, 'nemertea': [6217, 255, 'ribbon worms']
-, 'craniata': [115366, 46293, '?']
-, 'nematoda': [6231, 3356, 'roundworms']
-, 'monotremata': [9255, 5, '']
-, 'cycliophora': [69815, 2, 'symbion']
-, 'placozoa': [10226, 1, '?']
-, 'loricifera': [310840, 1, '?']
-, 'pycnogonida': [57294, 185, 'sea spiders']
-, 'brachiopoda': [7568, 100, 'lamp shells']
-, 'cephalochordata': [7735, 11, 'lancelets/amphioxus']
-, 'cnidaria': [6073, 3586, 'medusae/polyps']
-, 'entoprocta': [43120, 23, '?']
-, 'tardigrada': [42241, 168, '']
-, 'chaetognatha': [10229, 56, 'arrow worms']
-, 'bryozoa': [10205, 308, 'moss animals']
-, 'tunicata': [7712, 353, 'ascidians/sea squirts/tunicates/sea pork/sea livers/sea tulips']
-, 'porifera': [6040, 1318, 'sponges']
-, 'xiphosura': [6845, 5, 'horseshoe crabs']
-, 'cyclostomata': [1476529, 103, 'hagfish/lamprey']}
+#'arthropoda': [6656, 128142, '']
+#'mammalia': [40674, 5022, '']
+#'craniata': [115366, 46293, '?']
+#
+{'testudines': [8459, 353, 'turtles'], 'nematoda': [6231, 3356, 'roundworms'], 'platyhelminthes': [6157, 4171, 'flatworms'], 'tunicata': [7712, 353, 'ascidians/sea squirts/tunicates/sea pork/sea livers/sea tulips'], 'hexapoda': [6960, 106986, 'insects'], 'priapulida': [33467, 7, 'penis worms'], 'dipnoi': [7878, 6, 'lungfishes'], 'brachiopoda': [7568, 100, 'lamp shells'], 'nemertea': [6217, 255, 'ribbon worms'], 'lepidosauria': [8504, 6803, 'lizards/snakes'], 'myriapoda': [61985, 949, 'millipeds'], 'aves': [8782, 9339, 'birds'], 'crocodylia': [1294634, 24, 'crocodiles'], 'echinodermata': [7586, 1724, 'sea stars/urchins/cucumbers/lilies'], 'orthonectida': [33209, 2, '?'], 'gastrotricha': [33313, 118, 'hairybacks'], 'dicyemida': [10215, 24, '?'], 'cycliophora': [69815, 2, 'symbion'], 'eutheria': [9347, 4693, 'placentals'], 'cephalochordata': [7735, 11, 'lancelets/amphioxus'], 'phoroniformea': [120557, 14, 'horseshoe worms'], 'gnathostomulida': [66780, 21, 'jaw worms'], 'chaetognatha': [10229, 56, 'arrow worms'], 'bryozoa': [10205, 308, 'moss animals'], 'rotifera': [10190, 233, 'wheel animals'], 'xiphosura': [6845, 5, 'horseshoe crabs'], 'loricifera': [310840, 1, '?'], 'amphibia': [8292, 5520, ''], 'monotremata': [9255, 5, ''], 'ctenophora': [1003038, 57, 'comb jellies'], 'arachnida': [6854, 9644, 'spiders'], 'tardigrada': [42241, 168, ''], 'onychophora': [2074142, 89, 'velvet worms'], 'mollusca': [6447, 13501, ''], 'annelida': [6340, 3170, 'segmented worms'], 'micrognathozoa': [195505, 1, '?'], 'porifera': [6040, 1318, 'sponges'], 'actinopterygii': [7898, 18329, 'ray-finned fishes'], 'xenacoelomorpha': [1312402, 150, '?'], 'nematomorpha': [33310, 28, 'horsehair worms'], 'pycnogonida': [57294, 185, 'sea spiders'], 'cnidaria': [6073, 3586, 'medusae/polyps'], 'hemichordata': [10219, 39, 'acorn wormws'], 'placozoa': [10226, 1, '?'], 'crustacea': [6657, 10373, ''], 'metatheria': [9263, 324, 'marsupials'], 'kinorhyncha': [51516, 56, 'mud dragons'], 'entoprocta': [43120, 23, '?'], 'chondrichthyes': [7777, 812, 'cartilaginous fishes'], 'cyclostomata': [1476529, 103, 'hagfish/lamprey'], 'coelacanthimorpha': [118072, 2, 'lobe-finned fishes']}
