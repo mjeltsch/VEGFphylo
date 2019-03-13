80c10ed583b11a7b753cfd6892b949f32448fd0e
Jeltsch Michael <michael@jeltsch.org>
2019-03-13 06:49:39 +0200

    Added comments to data/master_dictionary.py

--- data/master_dictionary.py
+++ data/master_dictionary.py
@@ -3,12 +3,12 @@
 #
 # VEGF-A, PlGF, VEGF-B, VEGF-C, VEGF-D, VEGF-E, VEGF-F
 #
-# The value in the master dictionary is a list:
+# The value in the master dictionary is a list of five elements:
 #
 # 0 protein_id [fasta]
-# 1 subrange
+# 1 subrange (e.g. "50-200"; not used at the moment)
 # 2 number of blasts hist to return (default is 50 if None is given)
-# 3 list of alternative names
+# 3 list of alternative names (the first name is going to be the canonical ortholog group name)
 # 4 list of close relatives that can results in blast hits, that should be excluded
 #
 {'VEGF-C':["NP_005420.1", None, 20000, ['VEGF-C', 'vascular endothelial growth factor C'], ['platelet-derived growth factor', 'PDGF and VEGF related factor', 'vascular endothelial growth factor A', 'vascular endothelial growth factor B', 'vascular endothelial growth factor D', 'placenta growth factor']],
