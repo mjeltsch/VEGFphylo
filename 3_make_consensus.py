259b31c347756350e3a93b793321c1baef7cf7fa
Jeltsch Michael <michael@jeltsch.org>
2019-03-14 10:52:29 +0200

    More xml files and make_consensus.py almost done

--- 3_make_consensus.py
+++ 3_make_consensus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-import os, sqlite3, ntpath
+import os, sqlite3, ntpath, types, shutil
 from Bio import SeqIO
 from Bio import Entrez
 from pathlib import Path
@@ -120,6 +120,65 @@ def limit_to_most_informative_sequences(NUMBER, FASTA_FILE_IN, FASTA_FILE_OUT):
         'Reducing the complexity to the most informative {} sequences:'.format(NUMBER),
         't_coffee -other_pg seq_reformat -in {0} -action +trim _seq_n{1} -output fasta_seq > {2}'.format(FASTA_FILE_IN, NUMBER, FASTA_FILE_OUT))
 
+def concatenate_files(wildcard_path):
+    concatenated_file_name = wildcard_path.replace('*', 'all_')
+    execute_subprocess(
+        'Concatenate {0} into file {1}.'.format(wildcard_path, concatenated_file_name),
+        'cat {0} > {1}'.format(wildcard_path, concatenated_file_name))
+
+def do_alignment(file):
+    aligned_fasta_file = '{0}.aligned'.format(file)
+        execute_subprocess(
+            "Generating multiple sequence alignment with the following command:",
+            "t_coffee " + file + " -outfile " + aligned_fasta_file + " -output=fasta_aln -mode mcoffee")
+
+def trim_difficult_streches(file):
+    execute_subprocess(
+        'Gblocks of {0}.'.format(file),
+        # -b4 = minimum length of a block
+        # -b5 = allowed gap positions (none, half, all = n,h,a)
+        'Gblocks {0} -t=p -b4=10> -b5=h'.format(file))
+
+def encode_fasta_descriptions(fasta_file_name, list_file_name, encoded_aligned_fasta_file):
+    # Make code name list
+    execute_subprocess(
+        'Converting fasta descriptions part 1 (creating code list) with t_coffee using the following command:',
+        't_coffee -other_pg seq_reformat -in {0} -output code_name > {1}'.format(fasta_file_name, list_file_name))
+    # Encode fasta file
+    execute_subprocess(
+        'Converting fasta descriptions part 2 (replacing fasta descriptions with codes) with t_cofeee using the following command:',
+        't_coffee -other_pg seq_reformat -code {0} -in {1} > {2}'.format(list_file_name, fasta_file_name, encoded_aligned_fasta_file))
+
+def convert_into_phylip(file):
+    phylip_file = '{0}.phylip'.format(file)
+    execute_subprocess(
+        'Convert into phylip using the following command:',
+        't_coffee -other_pg seq_reformat -in {0} -output phylip_aln > {1}'.format(file, phylip_file)
+
+def make_tree(file):
+    # Detect whether parallel bootstrapping should be performed
+    mpirun_path = shutil.which('mpirun')
+    phymlmpi_path = shutil.which('phyml-mpi')
+    if mpirun_path != '' and phymlmpi_path != '':
+        phylo_command = 'mpirun -n 4 phyml-mpi -i {0} -d aa -b 100'.format(file)
+    else:
+        phylo_command = 'phyml -i {0} -d aa -b -1'.format(file)
+
+    execute_subprocess(
+        'Make tree with the following command:',
+        phylo_command)
+
+    # phyml adds or doesn't add the .txt extension to the output file (depending on the version) and we need to check for this!
+    phyml_output_file = '{0}_phyml_tree"
+    if os.path.isfile(phyml_output_file):
+        os.rename(phyml_output_file, '{0}.txt'.format(phyml_output_file))
+    return phyml_output_file
+
+def decode_fasta_descriptions(list_file, fasta_in_file, fasta_out_file):
+    execute_subprocess(
+        'Decoding tree file file into human-readable format using the following command:',
+        't_coffee -other_pg seq_reformat -decode {0} -in {1} {2}'.format(list_file, fasta_in_file. fasta_out_file))
+
 def run():
     global SEQUENCE_FILES, APPLICATION_PATH, MULTISEQUENCE_FASTA_DIR
     APPLICATION_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -147,15 +206,32 @@ def run():
     print('\nphyla: {0}'.format(counter_phyla))
     print('ortholog groups: {0}'.format(round(counter_orthologs/counter_phyla)))
     print('proteins: {0}'.format(counter_proteins))
-    # Reduce the number of sequences for each phylum/ortholog group combinbation to NUMBER
+
+    # Reduce the number of sequences for each phylum/ortholog group combinbation to the most
+    # informative NUMBER of sequences. Don't use to high NUMBER as the alignment and tree
+    # generation will take too long!
+    # Changing directory is necessary because t_coffee chockes if the file path gets too long
     os.chdir(MULTISEQUENCE_FASTA_DIR)
     NUMBER = 4
     for filename_in in os.listdir(MULTISEQUENCE_FASTA_DIR):
-        #filename_in = '{0}{1}'.format(MULTISEQUENCE_FASTA_DIR, filename_in)
         print('filename_in: {0}'.format(filename_in))
         filename_out = '../informative_multifasta_files/{0}'.format(filename_in)
         print('filename_out: {0}'.format(filename_out))
         limit_to_most_informative_sequences(NUMBER, filename_in, filename_out)
+        directory = '../informative_multifasta_files'
+
+    for ortholog in ortholog_group_list:
+        concatenate_files('{0}/*{1}.fasta'.format(directory, ortholog))
+        fasta_file_name = '{0}/all_{1}.fasta'.format(directory, ortholog))
+        do_alignment(fasta_file_name)
+        trim_difficult_streches(fasta_file_name)
+        list_file_name = '{0}/all_{1}.lst'.format(directory, ortholog))
+        encoded_aligned_fasta_file = '{0}/all_{1}.encoded'.format(directory, ortholog))
+        final_result = '{0}/final_{1}.fasta'.format(directory, ortholog))
+        encode_fasta_descriptions(fasta_file_name, list_file_name, encoded_aligned_fasta_file)
+        convert_into_phylip(encoded_aligned_fasta_file)
+        output_file = make_tree(encoded_aligned_fasta_file)
+        decode_fasta_descriptions(list_file_name, output_file, final_result)
 
 if __name__ == '__main__':
     run()
